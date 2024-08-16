1f6535c683301e9bef8c0de70ab232daa4befa60
Cristian Caceres <ccaceres@uqaidev.com>
2024-08-16 15:17:02 -0500

    build: agrega anotaciones para el funcionamiento de @loombook y refactoriza codigo

--- src/main/java/com/uqai/capacitacion/configuration/PropertiesConfig.java
+++ src/main/java/com/uqai/capacitacion/configuration/PropertiesConfig.java
@@ -1,22 +1,17 @@
 package com.uqai.capacitacion.configuration;
 
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Data
 @Component
 @ConfigurationProperties(prefix = "app")
 public class PropertiesConfig {
 
     private Map<String, String> email;
 
-    public Map<String, String> getEmail() {
-        return email;
-    }
-
-    public void setEmail(Map<String, String> email) {
-        this.email = email;
-    }
 }
--- src/main/java/com/uqai/capacitacion/controller/UserRestController.java
+++ src/main/java/com/uqai/capacitacion/controller/UserRestController.java
@@ -7,7 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -43,10 +49,10 @@ public class UserRestController {
     @GetMapping("/http-servlet-request")
     public UserDTO httpServletRequest(HttpServletRequest request) {
         var name = request.getParameter("name");
-        var lastName = request.getParameter("lastName");
+        var lastname = request.getParameter("lastName");
         var age = Integer.parseInt(request.getParameter("age"));
-        var email = request.getParameter("email");
-        return new UserDTO(name, lastName, age, email);
+        var emailRequest = request.getParameter("email");
+        return new UserDTO(name, lastname, age, emailRequest);
     }
 
     @GetMapping("/request-param")
